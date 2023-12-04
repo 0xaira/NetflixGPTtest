f8a8ccb308bd7f83456fa5699675cbbc0473bf4c
Aira <airajena07@gmail.com>
2023-12-04 18:02:22 +0530

    Form Validation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-[150px]">
       <img
         src={logo}
         alt=""
-        className="absolute top-0 left-0 z-10 w-44"
+        className="absolute top-0 left-0 z-10 "
       />
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useState , useRef} from "react";
 import Header from "./Header";
 import poster from '../assets/poster-bg.jpeg';
+import { checkValidData} from '../utils/validate';
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const email = useRef(null);
+  const password = useRef(null);
+  const [errorMessage, seterrorMessage] = useState(null);
+  const handleButtonClick =()=>{
+       const message=checkValidData(email.current.value,password.current.value);
+       seterrorMessage(message);
+  }
+
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
+
   return (
     <div>
       <Header />
       <div className="absolute">
-      <img src={poster} alt='poster' className='w-full' />
+        <img src={poster} alt='poster' className='w-full' />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -26,17 +39,34 @@ const Login = () => {
             className="p-4 my-4 w-full bg-gray-700"
           />
         )}
+
         <input
+        ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         />
         <input
+        ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+        {errorMessage && <p className='text-red-500 font-bold text-lg '>{errorMessage}</p>}
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="rememberMe"
+              className="mr-2"
+            />
+            <label htmlFor="rememberMe" className="text-gray-300">
+              Remember Me
+            </label>
+          </div>
+          <p className="text-gray-300 cursor-pointer">Need Help?</p>
+        </div>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
@@ -48,4 +78,5 @@ const Login = () => {
     </div>
   );
 };
+
 export default Login;
