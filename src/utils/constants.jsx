c15a1cc137710d50d3afb514e5f33eea1267868f
Aira <airajena07@gmail.com>
2023-12-06 18:16:33 +0530

    TMDB data fetch & Built Main Container

---
+++ src/utils/constants.jsx
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODA4NjM3M2FlN2VjMGYzMWI5ODJkMTY4YjJiMDcwMSIsInN1YiI6IjY1NzA0ZmY5NzlhMWMzMDBlMThlNjUyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O2pbbf-lLt-apmPK3O8H8MDDBu8qAXH2GpJRTIxJlBI'
+    }
+  };
