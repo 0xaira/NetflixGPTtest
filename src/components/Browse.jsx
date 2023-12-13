b9d83b30b385adc722e709e71ca8a7beea6b69be
Aira <airajena07@gmail.com>
2023-12-13 09:28:52 +0530

    Added Movie Card and Movie List

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -2,13 +2,17 @@ import { useEffect } from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
   return (
     <div>
       <Header />
       <MainContainer />
+      <SecondaryContainer/>
     </div>
   );
 };
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -5,3 +5,4 @@ export const API_OPTIONS = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODA4NjM3M2FlN2VjMGYzMWI5ODJkMTY4YjJiMDcwMSIsInN1YiI6IjY1NzA0ZmY5NzlhMWMzMDBlMThlNjUyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O2pbbf-lLt-apmPK3O8H8MDDBu8qAXH2GpJRTIxJlBI'
     }
   };
+  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
